4a2379476bfdb689fb8bba27dec1bb3c94bac616
Sayantan Sen <sayantansen530@gmail.com>
2024-07-03 09:45:44 +0530

    login_avatar_avatar_backend_added

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,6 +1,10 @@
-const { register } = require("../controllers/usersController");
+const { register, login, setAvatar, getAllUsers } = require("../controllers/usersController");
 
 const router=require("express").Router();
 
 router.post("/register",register);
+router.post("/login", login);
+router.post("/setavatar/:id", setAvatar);
+router.get("/allusers/:id", getAllUsers);
+
 module.exports=router;
