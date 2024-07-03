0106c7c3633b3d09aedf57e69e4598b8c75ce29f
Sayantan Sen <sayantansen530@gmail.com>
2024-07-03 19:32:13 +0530

    backend finish

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const userRoutes=require("./routes/userRoutes");
-const messageRoutes = require("./routes/messagesRoute");
+const authRoutes=require("./routes/auth");
+const messageRoutes = require("./routes/messages");
 const app = express();
 const socket = require("socket.io");
 require("dotenv").config();
@@ -10,7 +10,7 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/auth", userRoutes);
+app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 // mongoose
 //   .connect(process.env.MONGO_URL, {
