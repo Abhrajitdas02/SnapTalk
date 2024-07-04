15e5ad7b15ffe696a63fd640dac3ae4ec37c6fc4
Abhrajit Das <abhrajitdas12245@gmail.com>
2024-07-04 18:12:40 +0530

    link changes

--- server/index.js
+++ server/index.js
@@ -35,7 +35,7 @@ const server= app.listen(process.env.PORT,()=>{
 
 const io = socket(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://snap-talk-one.vercel.app",
     credentials: true,
   },
 });
